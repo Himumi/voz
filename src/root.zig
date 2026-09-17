9fe18080e84eff6e9d2ca05ecf9b07b158bd862c
himumi <erliansyaha@gmail.com>
2026-09-17 10:03:05 +0700

    create necessary files initializer

--- src/root.zig
+++ src/root.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const json = std.json;
 const Io = std.Io;
 
 pub const cli = @import("cli.zig");
@@ -36,3 +37,65 @@ pub const default_setting: Setting = .{
     .zls_version_url = "https://builds.zigtools.org/index.json",
     .local_versions = &.{},
 };
+
+pub const setting = "setting.json";
+pub const bin = "bin";
+pub const zig_versions = "zigVersions.json";
+pub const zls_versions = "zlsVersions.json";
+
+pub fn initFiles(io: Io) !void {
+    try initBin(io);
+    try initSetting(io);
+    try initZigVersions(io);
+    try initZlsVersions(io);
+}
+
+pub fn initBin(io: Io) !void {
+    const cwd = Io.Dir.cwd();
+    const dir = cwd.openDir(io, bin, .{}) catch |err| {
+        if (err != error.FileNotFound) return err;
+        return try cwd.createDir(io, bin, .default_dir);
+    };
+    defer dir.close(io);
+}
+
+pub fn initSetting(io: Io) !void {
+    const cwd = Io.Dir.cwd();
+    const union_file = cwd.openFile(io, setting, .{});
+
+    if (union_file) |file| {
+        file.close(io);
+    } else |err| {
+        if (err != error.FileNotFound) return err;
+
+        const new_file = try cwd.createFile(io, setting, .{});
+        defer new_file.close(io);
+
+        var buffer: [1024]u8 = undefined;
+        var file_writer = new_file.writer(io, &buffer);
+        const interface = &file_writer.interface;
+
+        try json
+            .Stringify
+            .value(default_setting, .{ .whitespace = .indent_2 }, interface);
+        try interface.flush();
+    }
+}
+
+pub fn initZigVersions(io: Io) !void {
+    const cwd = Io.Dir.cwd();
+    const file = cwd.openFile(io, zig_versions, .{}) catch |err| blk: {
+        if (err != error.FileNotFound) return err;
+        break :blk try cwd.createFile(io, zig_versions, .{});
+    };
+    defer file.close(io);
+}
+
+pub fn initZlsVersions(io: Io) !void {
+    const cwd = Io.Dir.cwd();
+    const file = cwd.openFile(io, zls_versions, .{}) catch |err| blk: {
+        if (err != error.FileNotFound) return err;
+        break :blk try cwd.createFile(io, zls_versions, .{});
+    };
+    defer file.close(io);
+}
